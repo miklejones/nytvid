52d6a58078abdd7656a09df322e064d910946f74
Michael Garland <michaelcgarland@gmail.com>
2018-03-22 14:48:22 -0400

    work on two computers

---
+++ nyt.js
@@ -0,0 +1,13 @@
+//SETUP VARIABLES
+//==================================================
+
+//FUNCTIONS
+//==================================================
+
+//MAIN PROCESSES
+//==================================================
+
+// 1. retriece user inputs and convert to variables
+// 2. use these variables to run an ajax call to the NYT
+// 3. break down the NYT object into useable fields
+// 4. dynamically generate the content for html 
